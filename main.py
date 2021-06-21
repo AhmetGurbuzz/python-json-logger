cbce92d69ccad452e0a47bc144fbdc03f7f13612
AhmetGurbuz <grbz523@gmail.com>
2021-06-21 22:25:13 +0300

    add: main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import json
+import logging
+
+
+class JsonFormatter(logging.Formatter):
+    def __init__(self):
+        super().__init__()
+
+    def format(self, record):
+        if isinstance(record, str):
+            return record.msg
+        else:
+            return record.msg.__str__()
+
+
+class Json:
+    def __init__(self,
+                 msg,
+                 payload=None):
+        self.msg = msg
+        self.payload = payload
+
+    def __str__(self):
+        return json.dumps(self.__dict__)
+
+
+if __name__ == '__main__':
+    logger = logging.getLogger(f'{__name__}')
+    logging.basicConfig(
+        format='[%(levelname)s] [%(pathname)s] [%(asctime)s] [%(name)s] %(message)s',
+        level=logging.INFO,
+        datefmt='%d/%m/%Y %I:%M:%S %p'
+    )
+
+    ch = logging.StreamHandler()
+    ch.setFormatter(JsonFormatter()), ch.setLevel(logging.INFO)
+    logger.addHandler(ch)
+
+    logger.info(Json(msg='message', payload={'key': 'value', '_key': '_value'}))
+
